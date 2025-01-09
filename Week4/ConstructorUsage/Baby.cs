ed69394e7fc4a68d09fd00d7f31cb2c3d4f38051
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-09 17:34:00 +0300

    Constructor lesson added.

---
+++ Week4/ConstructorUsage/Baby.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorUsage
+{
+    public class Baby
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Baby() 
+        {
+            //Default ctor
+        }
+
+        public Baby(string name, string surname) // Ctor with 2 parameters
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public void NewBaby()
+        {
+            BirthDate = DateTime.Now; // When method call then set birth date right that moment.
+            Console.WriteLine($"{Name} {Surname} isimli bebeğin doğum tarihi: {BirthDate}");
+            Console.WriteLine("Ingaaa!");
+        }
+    }
+}
---
+++ Week4/ConstructorUsage/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConstructorUsage;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Baby baby = new Baby();
+        baby.Name = "Ali";
+        baby.Surname = "Veli";
+
+        Baby baby2 = new Baby("Ayşe","Gül");
+
+        baby.NewBaby();
+        baby2.NewBaby();
+    }
+}
