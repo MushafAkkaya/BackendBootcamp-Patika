584d3c27014da5d37668b0e44c96e9a7f8d4d6a2
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-11 19:14:41 +0300

    AbstractionPractice added

---
+++ Week4/AbstractionPractice/Calisan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPractice
+{
+    public abstract class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        public abstract void Pozisyon();
+    }
+}
---
+++ Week4/AbstractionPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿using AbstractionPractice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Gorev gorev = new Gorev("Obi Wan", "Kenobi", "Eğitim", "Jedi");
+        gorev.Pozisyon();
+    }
+}
