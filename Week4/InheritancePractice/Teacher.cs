e0cb19a985472864a696e8e2c5579f8317934978
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-11 16:51:32 +0300

    InheritancePractice added.

---
+++ Week4/InheritancePractice/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    public class Teacher : BasePerson
+    {
+        public int Salary { get; set; }
+
+        public Teacher(string name, string surname, int salary)
+        {
+            Name = name;
+            Surname = surname;
+            Salary = salary;
+        }
+
+        public override void Person()
+        {
+            Console.WriteLine("Öğretmen adı, soyadı ve maaşı: " + Name + " " + Surname + " / " + Salary);
+        }
+    }
+}
