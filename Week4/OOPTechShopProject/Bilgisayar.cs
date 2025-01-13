915f7b996ed0df3a80dad511d69b2a4fd5a851a6
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-13 16:04:57 +0300

    OOPTechShopProject added

---
+++ Week4/OOPTechShopProject/Bilgisayar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTechShopProject
+{
+    class Bilgisayar : BaseMakine
+    {
+        private int _usbPortSayisi;
+        public bool BluetoothVarMi { get; set; }
+
+        public int UsbPortSayisi 
+        { 
+            get { return _usbPortSayisi; }
+            set 
+            {
+                if (value == 2 || value == 4)
+                {
+                    _usbPortSayisi = value;
+                }
+                else
+                {
+                    _usbPortSayisi = -1;
+                    Console.WriteLine("Usb portlarını 2 veya 4 tane olabilir!");//Kullanıcıdan belirli usb port sayıları alabilmek için encapsulation yapılır.
+                }
+            } 
+        }
+
+        public override void BilgileriYazdir()//Bilgisayar nesnesine göre metod ezilir.
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine($"Usb Port Sayisi: {UsbPortSayisi}");
+            if (BluetoothVarMi)
+            {
+                Console.WriteLine("Bluetooth: Var");
+            }
+            else
+            {
+                Console.WriteLine("Bluetooth: Yok");
+            }
+        }
+
+        public override void UrunAdiGetir()//BaseClasstan alınan abstract metod ezilir ve isteğe göre şekillendirilir.
+        {
+            Console.WriteLine($"Bilgisayarinizin adi: {Ad}");
+        }
+    }
+}
---
+++ Week4/OOPTechShopProject/Program.cs
@@ -0,0 +1,80 @@
+﻿using OOPTechShopProject;
+using System.Reflection.Metadata.Ecma335;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Teknoloji mağazasına hoşgeldiniz.");
+            Console.WriteLine("Telefon kaydi oluşturmak için 1'e, Bilgisayar kaydi oluşturmak için 2'ye basiniz.");
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            if (secim == 1)//Telefon için nesne üretilir ve bilgiler kullanıcıdan alınır.
+            {
+                Telefon telefon = new Telefon();
+
+                Console.Write("Ad: ");
+                telefon.Ad = Console.ReadLine();
+
+                Console.Write("Seri No: ");
+                telefon.SeriNo = Console.ReadLine();
+
+                Console.Write("Aciklama: ");
+                telefon.Aciklama = Console.ReadLine();
+
+                Console.Write("İsletim Sistemi: ");
+                telefon.IsletimSistemi = Console.ReadLine();
+
+                Console.Write("TR Lisanslı mı? (true/false): ");
+                telefon.Lisans = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine("\nÜrün başarıyla üretildi");
+
+                telefon.BilgileriYazdir();//BaseMakina sınıfından 2 metod çağrılarak bilgiler gösterilir.
+                telefon.UrunAdiGetir();
+            }
+            else if (secim == 2)//Bilgisayar için nesne üretilir ve bilgiler kullanıcıdan alınır.
+            {
+                Bilgisayar bilgisayar = new Bilgisayar();
+
+                Console.Write("Ad: ");
+                bilgisayar.Ad = Console.ReadLine();
+
+                Console.Write("Seri No: ");
+                bilgisayar.SeriNo = Console.ReadLine();
+
+                Console.Write("Aciklama: ");
+                bilgisayar.Aciklama = Console.ReadLine();
+
+                Console.Write("İsletim Sistemi: ");
+                bilgisayar.IsletimSistemi = Console.ReadLine();
+
+                Console.Write("Usb Port Sayisi: ");
+                bilgisayar.UsbPortSayisi = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Bluetooth var mi? (true/false): ");
+                bilgisayar.BluetoothVarMi = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine("\nÜrün başarıyla üretildi");
+
+                bilgisayar.BilgileriYazdir();//BaseMakina sınıfından 2 metod çağrılarak bilgiler gösterilir.
+                bilgisayar.UrunAdiGetir();
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz seçim yaptınız. Lütfen tekrar deneyin.");
+                continue;
+            }
+
+            Console.WriteLine("Yeni bir ürün üretmek ister misiniz? (evet/hayır)");
+            string devamDurumu = Console.ReadLine().ToLower();
+            if (devamDurumu != "evet")
+            {
+                Console.WriteLine("İyi günler dileriz.");
+                break;
+            }
+        }
+    }
+}
