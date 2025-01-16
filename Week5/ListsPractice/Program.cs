fc62e8b491432c5dbd6f1cd69f0944200bceb9e1
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-16 17:28:50 +0300

    Update Program.cs

--- Week5/ListsPractice/Program.cs
+++ Week5/ListsPractice/Program.cs
@@ -1,4 +1,5 @@
-﻿List<string> inviteList = new List<string>() //Davetli listesi tanımlanıp içine değerler eklenir.
+//PRACTICE 1
+List<string> inviteList = new List<string>() //Davetli listesi tanımlanıp içine değerler eklenir.
 {
     "Bülent Ersoy",
     "Ajda Pekkan",
@@ -13,6 +14,25 @@
 Console.WriteLine("** Davetliler **");
 
 foreach (var item in inviteList)
+{
+    Console.WriteLine(item);
+}
+
+
+//PRACTICE 2
+List<string> coffee = new List<string>(5);
+
+Console.WriteLine("Sırasıyla 5 kahve ismi giriniz.");
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Kahve {i}:");
+    coffee.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Girilen kahve isimleri:");
+
+foreach (var item in coffee)
 {
     Console.WriteLine(item);
 }
