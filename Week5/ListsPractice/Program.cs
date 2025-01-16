ba7490e376ae9cd2a41d2be282f7adddf415cb8e
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-16 17:17:55 +0300

    ListsPractice Added

---
+++ Week5/ListsPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> inviteList = new List<string>() //Davetli listesi tanımlanıp içine değerler eklenir.
+{
+    "Bülent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gündeş",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın"
+};
+
+Console.WriteLine("** Davetliler **");
+
+foreach (var item in inviteList)
+{
+    Console.WriteLine(item);
+}
