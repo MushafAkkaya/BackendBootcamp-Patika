c1436ec79ac6776c378006e559c27684dab707ef
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-17 16:21:57 +0300

    IMDBListProject added

---
+++ Week5/IMDBList/Film.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBList
+{
+    internal class Film
+    {
+        public string MovieName { get; set; }
+
+        public double ImdbScore { get; set; }
+
+        public Film(string movieName, double imdbScore)
+        {
+            MovieName = movieName;
+            ImdbScore = imdbScore;
+        }
+    }
+}
