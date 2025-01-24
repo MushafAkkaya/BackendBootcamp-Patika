5270198acf8a9a04ada67a760c40263df3e0d6bb
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-24 13:27:39 +0300

    SQLPractice5 added

---
+++ Week6/SQLPractice5/SQLPractice5.sql
@@ -0,0 +1,8 @@
+--1.madde
+select * from film where title like '%n' order by length desc limit 5
+
+--2.madde
+select * from film where title like '%n' order by length asc offset 5 limit 5
+
+--3.madde
+select * from customer where store_id = 1 order by last_name desc limit 4
