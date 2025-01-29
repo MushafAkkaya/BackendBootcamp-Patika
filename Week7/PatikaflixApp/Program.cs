7447a5c88c0f64fae95536310ea5979658f1a4fb
MushafAkkaya <90688694+MushafAkkaya@users.noreply.github.com>
2025-01-29 17:30:25 +0300

    PatikaflixApp added

---
+++ Week7/PatikaflixApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using PatikaflixApp;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Series> series = new List<Series>();
+
+        while (true)
+        {
+            Series newSeries = new Series();
+
+            Console.Write("Dizinin adını girin: ");
+            newSeries.Name = Console.ReadLine();
+
+            Console.Write("Dizinin yapım yılını girin: ");
+            newSeries.ReleaseDate = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Dizinin türünü girin: ");
+            newSeries.Type = Console.ReadLine();
+
+            Console.Write("Dizinin yönetmenini girin: ");
+            newSeries.Director = Console.ReadLine();
+
+            Console.Write("Dizinin yayınlandığı ilk platformu girin: ");
+            newSeries.Platform = Console.ReadLine();
+
+            series.Add(newSeries);
+
+            Console.WriteLine("Yeni dizi eklemek istiyor musunuz?(e/h)");
+            string answer = Console.ReadLine();
+
+            if (answer.Contains("h"))
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Eklenen diziler:");
+        foreach (var item in series)
+        {
+            Console.WriteLine($"Dizi adı: {item.Name} / Yılı: {item.ReleaseDate} / Türü: {item.Type} / Yönetmeni: {item.Director} / Platform: {item.Platform}");
+        }
+
+        List<NewSeries> comedySeries = series
+            .Where(x => x.Type.Equals("Komedi", StringComparison.OrdinalIgnoreCase))
+            .Select(x => new NewSeries { Name = x.Name, Type = x.Type, Director = x.Director })
+            .OrderBy(x => x.Name)
+            .ThenBy(x => x.Director)
+            .ToList(); // Komedi dizileri için yeni bir liste oluşturuldu ve kullanıcıdan alınan verilerle oluşturulmuş olan dizi listesinden istenilen sütunlar alındı.
+
+        Console.WriteLine("Komedi dizileri: ");
+        foreach (var item in comedySeries)
+        {
+            Console.WriteLine($"Adı: {item.Name} / Türü: {item.Type} / Yönetmen: {item.Director}");
+        }
+    }
+}
---
+++ Week7/PatikaflixApp/Series.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaflixApp
+{
+    internal class Series
+    {
+        public string Name { get; set; }
+        public int ReleaseDate { get; set; }
+        public string Type { get; set; }
+        public string Director { get; set; }
+        public string Platform { get; set; }
+    }
+}
