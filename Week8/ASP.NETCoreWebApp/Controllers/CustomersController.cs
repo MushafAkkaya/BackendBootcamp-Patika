d9c18ca6aa27ae2983d85eaa51e4fd7c27888686
MushafAkkaya <mushaf.fb@gmail.com>
2025-02-05 14:48:48 +0300

    ASP.NETCoreWebApp added

---
+++ Week8/ASP.NETCoreWebApp/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using ASP.NETCoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreWebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Jack",
+                LastName = "Sparrow",
+                Email = "jacksparrow@gmail.com"
+            };
+
+            var vievModel = new CustomerViewModel
+            {
+                Customer = customer,
+                WelcomeMessage = "Welcome to our customer portal!"
+            };
+
+            return View(vievModel);
+        }
+    }
+}
---
+++ Week8/ASP.NETCoreWebApp/Models/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NETCoreWebApp.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public string WelcomeMessage { get; set; }
+    }
+}
