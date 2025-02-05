442df9986e7ad2e6b2d344313bd041df79ee1225
MushafAkkaya <mushaf.fb@gmail.com>
2025-02-05 17:35:15 +0300

    BasicMVCPractice added

---
+++ Week8/BasicMVCPractice/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,37 @@
+﻿using BasicMVCPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicMVCPractice.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Ali",
+                LastName = "Veli",
+                Email = "aliveli@gmail.com"
+            };
+
+            var order = new Order
+            {
+                Id = 1,
+                ProductName = "Telefon",
+                Price = 50000,
+                Quantity = 1
+            };
+
+            List<Order> orders = new List<Order> { order };
+
+            var customerOrders = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+
+            return View(customerOrders);
+        }
+    }
+}
