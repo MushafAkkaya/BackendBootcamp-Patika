59f69bfdaafc4445246060fe4e85c2b654b1f54d
MushafAkkaya <mushaf.fb@gmail.com>
2025-03-17 10:27:20 +0300

    Week13 Added

---
+++ Week13/DependencyInjectionPractice/Models/Classroom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionPractice.Models
+{
+    internal class Classroom
+    {
+        private readonly Teacher _teacher;
+
+        public Classroom(Teacher teacher)
+        {
+            _teacher = teacher;
+        }
+
+        public string GetTeacherInfo()
+        {
+            return _teacher.GetInfo();
+        }
+    }
+}
