b59f87ff3e8f633608da0b0e3b4d41927e7ea576
MushafAkkaya <mushaf.fb@gmail.com>
2025-03-20 16:11:54 +0300

    Week13 updated

---
+++ Week13/DependencyInjectionPractice/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using DependencyInjectionPractice.Models;
+
+Teacher teacher = new Teacher("Ali", "Veli");
+
+Classroom classroom = new Classroom(teacher);
+
+Console.WriteLine(classroom.GetTeacherInfo());
