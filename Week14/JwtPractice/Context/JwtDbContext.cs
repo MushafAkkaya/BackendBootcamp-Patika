89f4e899b0725db802b8b85df9625f39f72c9257
MushafAkkaya <mushaf.fb@gmail.com>
2025-04-10 13:15:50 +0300

    JwtPractice Added

---
+++ Week14/JwtPractice/Context/JwtDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JwtPractice.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtPractice.Context
+{
+    public class JwtDbContext : DbContext
+    {
+        public JwtDbContext(DbContextOptions<JwtDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+    }
+}
---
+++ Week14/JwtPractice/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using JwtPractice.Context;
+using JwtPractice.Dtos;
+using JwtPractice.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IdentityAndDataProtectionPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtDbContext _context;
+        private readonly JwtHelper _jwtService;
+
+        public AuthController(JwtDbContext context, JwtHelper jwtService)
+        {
+            _context = context;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequestDto login)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == login.Email && x.Password == login.Password);
+
+            if (user == null)
+            {
+                return Unauthorized("User bilgisi bulunamadı.");
+            }
+
+            var token = _jwtService.GenerateToken(user);
+            return Ok(new { token });
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetEmail()
+        {
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+            return Ok();
+        }
+    }
+}
